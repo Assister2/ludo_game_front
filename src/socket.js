5adaefc257547f581dfb22e38e44fde9b522eb92
Assister2 <staniwitkow@gmail.com>
2023-07-28 02:12:44 +0500

    live socket

--- src/socket.js
+++ src/socket.js
@@ -2,10 +2,9 @@ import io from "socket.io-client";
 import Cookies from "js-cookie";
 
 const socketNew = io(process.env.REACT_APP_CLIENT_BASEURL_WS, {
-  // autoConnect: true,
+  autoConnect: true,
   auth: {
     token: `${Cookies.get("token")}`,
-    userId: `${Cookies.get("userId")}`,
   },
 });
 export default socketNew;
